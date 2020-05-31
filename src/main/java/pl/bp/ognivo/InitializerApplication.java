e4e23ce1fb42b5a983e02b343613804ee6adb003
Artur Szatkowski <aszatkowski@Mac-mini-1.home>
2020-05-31 19:01:18 +0200

    Initial commit

---
+++ src/main/java/pl/bp/ognivo/InitializerApplication.java
@@ -0,0 +1,13 @@
+package pl.bp.ognivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bp/ognivo/InitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bp.ognivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
