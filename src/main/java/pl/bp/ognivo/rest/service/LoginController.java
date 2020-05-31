cfa25196f2f6ead643e50b63c3786b26822de632
aszatkowski <szatkowski.artur@gmail.com>
2020-05-31 23:09:04 +0200

    ADD: authenticate method with JWT hardcoded.

--- src/main/java/pl/bp/ognivo/rest/service/LoginController.java
+++ src/main/java/pl/bp/ognivo/rest/service/LoginController.java
@@ -1,15 +1,35 @@
 package pl.bp.ognivo.rest.service;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 public class LoginController {
 
-    @PostMapping("/login")
-    public String login(@RequestParam String login, @RequestParam String password) {
+    @PostMapping("/authenticate")
+    public String authenticate(HttpServletResponse response, @RequestParam String login, 
+    @RequestParam String password) {
+        String token = produceToken(login);
+        response.addHeader("token", token);
         return "Login is: " + login + ", password is: " + password;
     }
 
+    //TODO: wynieść jako generyczne
+    private String produceToken(String login){
+        Long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(login)
+                .claim("roles", "user")
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + 10000)) //TODO: wynieść do parametrów
+                .signWith(SignatureAlgorithm.HS512, "secretkey").compact(); //TODO: klucz wygenerować i do parametrów
+
+    }
 }
